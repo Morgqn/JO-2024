5cf91badd18411f2d3543bb23b40007410f8c817
Tobias <Sullivan>
2017-12-30 18:44:31 +0100

    Js du formulaire

--- JO2024/Pages/form.php
+++ JO2024/Pages/form.php
@@ -4,6 +4,7 @@
 </script-->
 <link href="style.css/Inscription.css" rel="stylesheet">
 
+
 <form class="modal multi-step" id="demo-modal-3">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -30,23 +31,23 @@
             </div>
             <center>
                 <div class="modal-body step-1" data-step="1">
-                    <p><input class="fInput" placeholder="First name..."></p>
-                    <p><input class="fInput" placeholder="Last name..."></p>
+                    <p><input class="fInput" placeholder="First name..." id="surname" onblur="formatSurname(this)"></p>
+                    <p><input class="fInput" placeholder="Last name..." id="name" onblur="formatName(this)"></p>
                     <p>
-                        <select class="fSelect">
+                        <select class="fSelect" id="selectGender">
                         <option>Male</option>
                         <option>Female</option>
                     </select></p>
                 </div>
             </center>
             <div class="modal-body step-2" data-step="2">
-                <p><input class="fInput" type="mail" placeholder="Email"></p>
-                <p><input class="fInput" placeholder="Phone"></p>
+                <p><input class="fInput" type="mail" placeholder="Email" id="mail" onblur="isEmail(this)"></p>
+                <p><input class="fInput" placeholder="Phone" id="phone" onblur="isPhone(this)"></p>
             </div>
             <div class="modal-body step-3" data-step="3">
-                <p><input type="date" class="fInput" placeholder="dd"></p>
-                <p><input class="fInput" placeholder="Username..."></p>
-                <p><input class="fInput" placeholder="Password..."></p>
+                <p><input type="date" class="fInput" placeholder="dd" id="date" onblur="isDate(this)"></p>
+                <p><input class="fInput" placeholder="Username..." id="username" onblur="isUsername(this)"></p>
+                <p><input class="fInput" placeholder="Password..." id="pass" onblur="isPass(this)"></p>
             </div>
             <div class="modal-body step-4" data-step="4">
                 <p><input type="file" class="fInput"></p>
@@ -54,12 +55,12 @@
             </div>
             <div class="modal-footer">
 
-                <button type="button" class="btn btn-primary step step-2" data-step="2" onclick="sendEvent('#demo-modal-3', 1)">Back</button>
-                <button type="button" class="btn btn-primary step step-1" data-step="1" onclick="sendEvent('#demo-modal-3', 2)">Continue</button>
-                <button type="button" class="btn btn-primary step step-3" data-step="3" onclick="sendEvent('#demo-modal-3', 2)">Back</button>
-                <button type="button" class="btn btn-primary step step-2" data-step="2" onclick="sendEvent('#demo-modal-3', 3)">Continue</button>
-                <button type="button" class="btn btn-primary step step-3" data-step="3" onclick="sendEvent('#demo-modal-3', 4)">Continue</button>
-                <button type="button" class="btn btn-primary step step-4" data-step="4" onclick="sendEvent('#demo-modal-3', 3)">Back</button>
+                <button type="button" class="btn btn-primary step step-2" data-step="2" onclick="sendEvent2('#demo-modal-3', 1)">Back</button>
+                <button type="button" class="btn btn-primary step step-1" data-step="1" onclick="sendEvent1('#demo-modal-3', 2)">Continue</button>
+                <button type="button" class="btn btn-primary step step-3" data-step="3" onclick="sendEvent3('#demo-modal-3', 2)">Back</button>
+                <button type="button" class="btn btn-primary step step-2" data-step="2" onclick="sendEvent2('#demo-modal-3', 3)">Continue</button>
+                <button type="button" class="btn btn-primary step step-3" data-step="3" onclick="sendEvent3('#demo-modal-3', 4)">Continue</button>
+                <button type="button" class="btn btn-primary step step-4" data-step="4" onclick="sendEvent3('#demo-modal-3', 3)">Back</button>
                 <button type="button" class="btn btn-primary step step-4" data-step="4">Submit</button>
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
@@ -68,10 +69,25 @@
 </form>
 
 <script src="multi-step-modal.js"></script>
+<script src="JS_JO/inscription.js"></script>
 
 <script>
-    sendEvent = function(sel, step) {
-        $(sel).trigger('next.m.' + step);
+    sendEvent1 = function(sel, step) {
+        if (verifFirstPage(this) === true) {
+            $(sel).trigger('next.m.' + step);
+        }
+    }
+    
+    sendEvent2 = function(sel, step) {
+        if (verifSecondPage(this) === true) {
+            $(sel).trigger('next.m.' + step);
+        }
+    }
+    
+    sendEvent3 = function(sel, step) {
+        if (verifThirdPage(this) === true) {
+            $(sel).trigger('next.m.' + step);
+        }
     }
 
 </script>
