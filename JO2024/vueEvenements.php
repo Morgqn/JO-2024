6f56e57c5da91b49194fd1810fe022f5c46d5787
houdaBEN <houda.benmakhlouf.kf@gmail.com>
2018-01-03 10:43:23 +0100

    displaying evenement format master detail

--- JO2024/vueEvenements.php
+++ JO2024/vueEvenements.php
@@ -2,13 +2,17 @@
     <?php
     include("controller.php");
     $uncontroleur = new Controller("localhost","JObdd","root","root");
-$uncontroleur->setTable("Evenement");
-//$resultat = $uncontroleur->selectAll2(); 
+    $uncontroleur->setTable("Evenement");
+    //$resultat = $uncontroleur->selectAll2(); 
     $tab = array("ID_EVENEMENT"=>$_GET['id']);
     $resultat = $uncontroleur->selectwhere($tab);
+    $links =  $resultat['IMAGES'];
+    $links_array = explode(",", $links);
+
 
      // echo "<tr><td>".$unResultat['DATE']."</td><td>".$unResultat['LIBELLE_LIEU']."</td><td>".$unResultat['ADRESSE']."</td><td>".$unResultat['TIME']."</td><td>".$unResultat['LIBELLE_EVENEMENT']."</td></tr>";
         echo "la date de l'evenement : ".$resultat['DATE'].'<br>';
+        $date = $resultat['DATE'];
         echo "Heure : ".$resultat['TIME'].'<br>';
         
         $uncontroleur->setTable("Lieu");
@@ -16,6 +20,15 @@ $uncontroleur->setTable("Evenement");
         $resultat = $uncontroleur->selectwhere($tab);
         echo "le lieu de l'evenement : ".$resultat['LIBELLE'].'<br>';
         echo "l'adresse de l'evenement : ".$resultat['ADRESSE'].'<br>';
+
+        //var_dump($links_array);
+        echo "les images dans l'evenement :";
+
+        foreach ($links_array as $value) {
+            $src = 'evenement_images/'.$date.'/'.$resultat['LIBELLE'].'/'.$value;
+            echo '<img src="'.$src.'">';
+        }
+       
 
 
 
