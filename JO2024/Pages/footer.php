fc28dd7bd9c74f30f3cf71f4e991daabe4ba3bbd
Morgan <fulgoni.morgan@gmail.com>
2018-01-23 03:18:06 +0100

    Nouveau footer

--- JO2024/Pages/footer.php
+++ JO2024/Pages/footer.php
@@ -2,24 +2,24 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 mb-5 mb-lg-0">
-                <h4 class="text-uppercase mb-4">Médias</h4>
-                <p class="lead mb-0"><a href="index.php">DERNIÈRES NEWS</a></p>
-                <p class="lead mb-0"><a href="#">CALENDRIER D'ÉVÉNEMENTS</a></p>
-                <p class="lead mb-0"><a href="galerie.php">PHOTOS</a></p>
+                <h4 class="text-uppercase mb-4" style="font-family:initial!important;font-size:20px;">Médias</h4>
+                <p class="lead mb-0"><a href="index.php"style="font-family:initial!important;font-size:15px;font-style: italic;">DERNIÈRES NEWS</a></p>
+                <p class="lead mb-0"><a href="#"style="font-family:initial!important;font-size:15px;font-style: italic;">CALENDRIER D'ÉVÉNEMENTS</a></p>
+                <p class="lead mb-0"><a href="galerie.php"style="font-family:initial!important;font-size:15px;font-style: italic;">PHOTOS</a></p>
                 <br></p>
             </div>
             <div class="col-md-4 mb-5 mb-lg-0">
-                <h4 class="text-uppercase mb-4">PROGRAMMES</h4>
-                <p class="lead mb-0"><a href="#">SPORT POUR L'ESPOIR</a></p>
-                <p class="lead mb-0"><a href="#">FEMME ET SPORT</a></p>
-                <p class="lead mb-0"><a href="Sports.php">SPORTS</a></p>
+                <h4 class="text-uppercase mb-4"style="font-family:initial!important;font-size:20px;">PROGRAMMES</h4>
+                <p class="lead mb-0"><a href="#"style="font-family:initial!important;font-size:15px;font-style: italic;">SPORT POUR L'ESPOIR</a></p>
+                <p class="lead mb-0"><a href="#"style="font-family:initial!important;font-size:15px;font-style: italic;">FEMME ET SPORT</a></p>
+                <p class="lead mb-0"><a href="Sports.php"style="font-family:initial!important;font-size:15px;font-style: italic;">SPORTS</a></p>
                 <br></p>
             </div>
             <div class="col-md-4">
-                <h4 class="text-uppercase mb-4">JEUX OLYMPIQUES</h4>
-                <p class="lead mb-0"><a href="#">LES JEUX D'ÉTÉ</a></p>
-                <p class="lead mb-0"><a href="#">LES JEUX D'HIVER</a></p>
-                <p class="lead mb-0"><a href="Resultats.php">RÉSULTATS</a></p>
+                <h4 class="text-uppercase mb-4"style="font-family:initial!important;font-size:20px;">JEUX OLYMPIQUES</h4>
+                <p class="lead mb-0"><a href="#"style="font-family:initial!important;font-size:15px;font-style: italic;">LES JEUX D'ÉTÉ</a></p>
+                <p class="lead mb-0"><a href="#"style="font-family:initial!important;font-size:15px;font-style: italic;">LES JEUX D'HIVER</a></p>
+                <p class="lead mb-0"><a href="Resultats.php"style="font-family:initial!important;font-size:15px;font-style: italic;">RÉSULTATS</a></p>
             </div>
         </div>
 </footer>
